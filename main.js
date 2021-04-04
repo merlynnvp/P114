ff61755e141f9986bc23e220d438742528f8e0bc
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-04-04 17:39:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() 
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(400, 300);
+    canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
